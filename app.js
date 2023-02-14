5d39ce626222358522d28c5d99a506454c72cd4a
Anas khan <anaskhan41347@gmail.com>
2023-02-15 00:21:45 +0500

    Java script Practice

---
+++ app.js
@@ -0,0 +1,177 @@
+// CHAPTER NUMBER 1
+
+
+// QUEST NO 1
+
+// alert("greet your website");
+
+// // QUEST NO 2
+
+
+// alert(" ERROR! PLEASE ENETR A VALID PASSWORD ");
+
+
+// QUEST NO 3
+
+
+// var line1 = "welcome to js land"
+// var line2 = "happy to coding"
+//  var message = Line1+ "\n" + line2;
+// function lineBreak(){
+// alert ("happy to  coding");
+// }
+
+
+// // QUEST NO 4
+
+// var state = ("Hello.. I can Run JS Through ,My Web Browser console.");
+// console.log("Hello.. I can Run JS Through ,My Web Browser console.");
+// alert(
+//     "Hello.. I can Run JS Through ,My Web Browser console"
+// )
+
+
+
+// // QUEST NO 6
+
+
+
+
+// var line1 = " Using Javascript in html and css"
+// var line2 = " Using Javascript in html and css"
+//  var message = Line1+ "\n" + line2;
+// function lineBreak(){
+// alert (" Using Javascript in html and css ");
+// }
+
+                                           
+
+  // CHAPTER NUMBER 2
+//   QUEST NO 1
+
+// var  username =("userName");
+// console.log("userName")
+
+
+//   QUEST NO 2
+
+// var myName = ("Full Name");
+// console.log("Full Name");
+
+
+//   QUEST NO 3
+
+// var message =("Hello world")
+// alert("Hello world")
+
+// QUEST NO 4
+
+// var studentsdata = ("Jhone Doe");
+// alert("Jhone Doe")
+
+// var studentsdata = ("Certified Mobile Application Development");
+// alert("Certified Mobile Application Development")
+
+// var studentsdata = 18;
+// alert(18)
+
+
+// QUEST NO 4
+// var text =
+// "Pizza\nPizz\nPiz";
+
+// function gfg_Run() {
+// alert(text);
+// }
+
+
+// / QUEST NO 5
+
+
+// var email = ("example@gmail.com"+"ANAS@gmail.com" )
+// alert("example@gmail.com"+"ANAS@gmail.com")
+
+
+// QUEST NO 6
+
+// var book =("A SMARTER LEARN TO WAY A JAVA SCRIPT");
+// alert("A SMARTER LEARN TO WAY A JAVA SCRIPT")
+
+// QUEST NO 7
+
+// var value = ("Yeah I Can Write HTML Content throgh Java");
+// document.write("Yeah I Can Write HTML Content throgh Java")
+
+// QEST NO 8
+
+// var varName =("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”");
+// alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+
+
+// CHAPTER NUMBER 3
+// QEST NO 1
+// var Age =("Im 15 years old");
+// alert("Im 15 years old")
+
+
+// QUEST NO 2
+// var timeavisitorhasvisitedawebpag =("You Have Visited 17 times")
+// alert("“You Have Visited 17 times")
+
+
+// QUEST NO 3
+
+// var birthYear = ("birthYear");
+// document.write("My Birthday Is 1990 ")
+// document.write("Data Type of my Variable is Number")
+
+// QUEST NO 4
+
+// var VisitorsName= ("John Doe");
+// var Producttitle= ("John Doe");
+// var  howmanyproductavisitorwantstoorder= ("John Doe");
+
+// var sumofall = ("VisitorsName+Producttitle+ howmanyproductavisitorwantstoorder")
+// document.write(" jhon Doe ordered  5 T-shirt(s) on XYZ Clothing store ")
+
+
+
+
+// CHAPTER NUMBER 3
+
+
+
+// quest no 1
+
+// var UserAge1= 19;
+// document.write("19  ")
+
+
+// let UserAge2= 19;
+// document.write("19 ")
+
+// const UserAge3= 19;
+// document.write("19")
+
+
+// quest no 2
+
+// Legal
+
+// var userName
+// var UserName
+// var username
+// var username99
+// var user99name99
+
+
+// / Not Legal
+
+// var 99user99name
+// var &username99
+// var &username99
+
+
+// quest no 3
+
+
