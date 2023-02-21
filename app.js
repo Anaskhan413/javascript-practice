7fc486d529c9d062db2ffb974ae76fb4cccb364f
Anas khan <anaskhan41347@gmail.com>
2023-02-22 02:00:26 +0500

    Java Script

--- app.js
+++ app.js
@@ -137,7 +137,7 @@
 
 
 
-// CHAPTER NUMBER 3
+// CHAPTER NUMBER 4
 
 
 
@@ -172,6 +172,279 @@
 // var &username99
 
 
-// quest no 3
+// CHAPTER NUMBER 3
+//  Question 1 
+
+// var numbver1 = 3;
+// var numbver1 = 5;
+
+// var sumofnum1num2 = (3 + 5);
+// console.log("8")
+
+
+// question 2
+
+// var numbver1 = 3;
+// var numbver1 = 5;
+
+// var sumofnum1num2 = (5 - 3);
+// console.log("2")
+
+
+// question 3
+
+// var numbver1 = 3;
+// var numbver1 = 5;
+
+// var sumofnum1num2 = (5 * 3);
+// console.log("15")
+
+
+
+// question 3
+
+// var numbver1 = 3;
+// var numbver1 = 5;
+
+// var sumofnum1num2 = (5 / 3);
+// console.log("1")
+
+
+
+// question 4
+
+// var ticket = 600;
+// var total = 600 * 5;
+// document.write("3000");
+
+
+// question 4
+
+// var value1 = ("4*1=4");
+// Document.write=("value1")
+
+
+
+// ((chapeter 5))
+// question 1
+
+// var value1 = +prompt(" number you want to + ");
+// var value2 = +prompt(" number you want to +");
+// var sum = value1 + value2
+// document.write(  value1 + " and " + value2  )
+
+// 
+// var value1 = +prompt("number you want to -");
+// var value2 = +prompt(" number you want to -");
+// var sum = value1 - value2
+// document.write(  value1 "and "- value2  )
+
+
+// 
+// var value1 = +prompt("add number you want to multiplication ");
+// var value2 = +prompt("add another number you want to multiplication");
+// var sum = value1 * value2
+
+
+// var value1 = +prompt("you want to divide ");
+// var value2 = +prompt(" number you want to divide");
+// var sum = value1 / value2
+
+
+// question 3
+
+
+// var a = undefined;
+// document.write(" variable Value declaration is: " + a); a = 5;
+// 
+// document.write("Initial value:" + a); a++;
+// 
+// document.write("Value after increment is: " + a);
+// a +=7;
+// document.write("Value after addition is: " + a);
+// a--;
+
+
+// question 4
+
+// var Tickets = 600;
+// var total Tickets = ticket * 5 ;
+// document.write(" 5 tickets buyy " + total )
+
+// question 5
+
+// var totalMarks = 980;
+// var marksObtained = 804;
+// var percentage = 100*(TotalMarks/obtainedmask);
+// document.write("Total marks" + totalMarks);
+
+// question 6
+
+// var NumBER = 5;
+// var results =  10 / 2 * (Num + 5)  ;
+// document.write(results)
+
+// question 7
+// var currentYear = 2023;
+// var birthYear = +prompt("Type your birth year")
+// var age = currentYear - birthYear;
+// document.write(results)
+
+
+// ((Chapter #6-9)) 
+// question 1
+// var a = 10;
+// document.write("<h2>Result:</h2>")
+// document.write(" Value of a is: " + a)
+// document.write("<br><br Value of ++a is + ++a)
+// document.write("<br>Now value of a is +a)
+// document.write("<br><br>  value of a++ is  + a++)
+// document.write("<br>Now  value of a is  +a)
+
+
+// question 3
+
+// var a = 2 ;
+// var b = 1 ; 
+// var result = --a - --b + ++b + b--; 
+
+//  a=2
+// --a; 
+// pre-deceremnt a=1
+
+// --a - --b + ++b; 1 - 1 = 0
+// document.write("a + ++a)
+// document.write("b  " +  ++b)
+
+// question 4
+
+// var user = prompt("What'S is your name")
+// document.write("Greets to You " + user)
+
+// question 5
+
+// var subject1 = prompt("Put the name of first subject: ");
+// var subject2 = prompt("Put the name of sec subject: ");
+// var subject3 = prompt("Put the name of thir subject: ");
+// var totalMarks = 100;
+
+// var obtainedMarks1 = +prompt("Enter your marks for "subject1");
+// var obtainedMarks2 = +prompt("Enter your marks for "subject2");
+// var obtainedMarks3 = +prompt("Enter your marks for "subject3");
+// let totaLMARKS  =" obtainedMarks1" +" obtainedMarks1" + " obtainedMarks1";
+// var Percentage = 100*(totaLMARKS/300);
+// document.write("Here is Your Total marks")
+
+
+
+// Chapter #9-11
+
+// question 1
+// var city = prompt("Your City Name?")
+// if(city == "Karachi"){
+//     alert("Welcome To The City ")
+// }else{
+//     alert("nOT welcome")
+// }
+
+
+// question 2
+
+// var gender = prompt("Type Your Gender")
+// if(gender == "male"){
+//     alert("Good Morning")
+// }
+// }else{
+//     alert("Hello!")
+// }
+
+// question 3
+
+// var signal = prompt("Show Signal Color.")
+// if(signal == "Red"){
+//     alert("Stop!")
+// }else{
+//     alert("select Write  right color!!!")
+// }
+
+
+// var fuel = +prompt("Whats the remainaing quantity og fuel in your car!")
+// if(fuel < 0.25){
+//     alert(" Fill fuel i")
+// }else{
+//     alert("not goof")
+
+// }
+
+
+
+// question 5
+// Question #6
+
+// var Subject1 = prompt("Obtaines marks obtained in Subject1: ");
+// var Subject2 = prompt(" Obtained Marks in Subject2: ");
+// var Subject3 = prompt("Obtaines marks obtained in Subject3: ");
+// var TotalMarks = prompt("Obtaines Total Marks: ");
+// var Percentage = (ObtainedMarks/TotalMarks)*100;
+// document.write("Total Marks: " + TotalMarks + );
+
+// if (Percentage >= 80) {
+//     document.write("A+");
+// }else{
+//     document.write("Sorry you Failed.")
+// }
+
+
+// Question #7
+
+// const secret= 5;
+// const guess = prompt("search number between 1 to  10");
+
+// if (guess == secretNumber) {
+//     document.write("Hurry Correct Answer");
+// } else {
+//     document.write("Wrong answer!");
+// }
+
+// Question #8
+
+
+// const numberdivsion = +prompt("Please Enter number");
+// if(divisionNumber == 0){
+//     document.write( divisionNumber + " is divisible by 3");
+
+// Question #8
+
+// let userInput = +prompt("Please enter a number:");
+// if (userInput % 2 === 0) {
+//     document.write("number " + " Even number");
+// } else {
+//     document.write("number "  + " Odd number.");
+
+
+// Question #8
+// var temp = +prompt("Submit Temperature: ")
+// if(temp > 40){
+//     document.write("Ihot outside.")
+// } else {
+//     document.write("OMG! Today's weather is so Cool.")
+// }
+// }
+
+
+// Question #9
+
+// var number1 = +prompt("ENTER NUMBER 1")
+// var number2 = +prompt("ENTER NUMBER 2")
+// var opt = prompt("ENTER opt")
+
+
+// if (opt === "+") {
+//     document.write(number1+ number2)
+// } else if (opt === "-") {
+//     document.write(number1 - number2)
+// } else if (opt === "*") {
+//   
+
 
 
